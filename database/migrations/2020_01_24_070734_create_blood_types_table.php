94125379d10930927c4ac1b04faad30ad5fab1d1
Sahrun <=sahrun.raja.tega@gmail.com>
2020-01-24 14:25:58 +0700

    schema table blood_types

---
+++ database/migrations/2020_01_24_070734_create_blood_types_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBloodTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blood_types', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('name');
+            $table->boolean('is_active');
+            $table->string('description')->nullable();
+            $table->integer('created_by');
+            $table->integer('modified_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blood_types');
+    }
+}
